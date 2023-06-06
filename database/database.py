ddb0a57373061ab18d079ec993a5fd66ed6ef95e
Dave Westerveld <davewesterveld@gmail.com>
2023-06-06 16:58:34 -0400

    add profile endpoint and update page

--- database/database.py
+++ database/database.py
@@ -1,9 +1,10 @@
 import os
 from enum import Enum
+from typing import Optional
 
 from sqlalchemy import Integer, create_engine
-from sqlalchemy.orm import (Mapped, declarative_base, mapped_column,
-                            sessionmaker)
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column, sessionmaker
+
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
@@ -20,9 +21,21 @@ class Status(Enum):
 
 
 class Task(Base):
-    __tablename__ = 'task'
+    __tablename__ = "task"
 
     id = mapped_column(Integer, primary_key=True)
 
     description: Mapped[str]
     status: Mapped[Status]
+
+
+class User(Base):
+    __tablename__ = "profile"
+
+    id = mapped_column(Integer, primary_key=True)
+
+    username: Mapped[str]
+    md5_password_hash: Mapped[str]
+    email: Mapped[Optional[str]]
+    full_name: Mapped[Optional[str]]
+    disabled: Mapped[Optional[bool]]
